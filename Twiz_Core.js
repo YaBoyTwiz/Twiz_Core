bb888b4fa4e267609f40e5a305e766e51780dd70
YaBoyTwiz <itsyaboytwiz@protonmail.com>
2023-09-14 20:55:10 -0500

    test

---
+++ Twiz_Core.js
@@ -0,0 +1,4 @@
+Hooks.on("renderActorSheet", (app, html, data) => {
+    console.log("An Actor sheet was rendered!xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+    // Your custom script or function goes here
+});
